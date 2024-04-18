b026dbc1192220203bc40f663939f9d96b2ff5b3
alekha-rao1 <alekrao@gmail.com>
2024-04-18 13:53:07 -0400

    starting header php/styling

---
+++ header.php
@@ -0,0 +1,31 @@
+<!-- 
+    Authors: Cordelia Ludden, Alekha Rao, (ADD NAMES HERE)
+ -->
+ <!DOCTYPE html>
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="styles.css">
+
+     <title>Secondhand Stories - About Us</title>
+     <style>
+
+     </style>
+
+ </head>
+ <body>
+
+    <nav>
+        <ul class="navbar">
+            <li class="logo"><a href="home_page.html"><img src="logo.png"></a></li>
+            <li><a href="browse.php">Browse</a></li>
+            <li><a href="sell.php">Browse</a></li>
+            <li><a href="about_us.html">About</a></li>
+            <input type="text" placeholder="Search Title, Author, Keyword, or IBSN">
+            <li class="cart"><a href="cart.php"><img src="cart.png"></a></li>
+            <li class="user"><a href="user.php"><img src="user.png"></a></li>
+            
+        </ul>
+    </nav>
+ </body>
+ </html>
