ebf51868c303790d0f5e92cde89e0543513174c3
alekha-rao1 <alekrao@gmail.com>
2024-04-18 14:46:32 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,14 @@
 <!-- 
-
  -->
  <!DOCTYPE html>
  <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
      <style>
         body {
     margin: 0;
-    font-family: latoReg;
-    src: url(Lato-Regular.ttf);
+    font-family:serif;
 }
 
 nav {
@@ -45,10 +44,14 @@ nav {
     margin-right: 10px;
 }
 
-.navbar li img {
-    max-height:40px;
-}
+.navbar li.logo img {
+    width:400px !important;
 
+}
+.navbar li.user img,
+.navbar li.cart img {
+    max-height: 40px;
+}
 .navbar li a {
     color: #2D446E;
     text-decoration: none;
@@ -57,6 +60,36 @@ nav {
 .navbar li a:hover {
     color: #2D446E;
     text-decoration: underline;
+}
+
+
+/* Hide the link that should open and close the topnav on small screens */
+.navbar .icon {
+  display: none;
+}
+
+/* When the screen is less than 600 pixels wide, hide all links, except for the first one ("Home"). Show the link that contains should open and close the topnav (.icon) */
+@media screen and (max-width: 600px) {
+  .navar a:not(:first-child) {display: none;}
+  .navbar a.icon {
+    float: right;
+    display: block;
+  }
+}
+
+/* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
+@media screen and (max-width: 600px) {
+  .navbar.responsive {position: relative;}
+  .navbar.responsive a.icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .navbar.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
 }
     </style>
 
@@ -69,7 +102,7 @@ nav {
  <body>
 
     <nav>
-        <ul class="navbar">
+        <ul class="navbar" id="nav">
             <li class="logo"><a href="home_page.html"><img src="logo.png"></a></li>
             <li><a href="browse.php">Browse</a></li>
             <li><a href="sell.php">Sell</a></li>
@@ -77,8 +110,22 @@ nav {
             <input type="text" placeholder="Search Title, Author, Keyword, or IBSN" size="50">
             <li class="cart"><a href="cart.php"><img src="cart.png"></a></li>
             <li class="user"><a href="user.php"><img src="user.png"></a></li>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
             
         </ul>
     </nav>
+
+    <script>
+        function myFunction() {
+            var x = document.getElementById("nav");
+            if (x.className === "navbar") {
+                x.className += " responsive";
+            } else {
+                x.className = "navbar";
+            }
+        }
+    </script>
  </body>
  </html>
