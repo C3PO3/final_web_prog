8f9401c6f8718ce128cd4b9d0643c1ffa2b55186
laurenwu22 <158392472+laurenwu22@users.noreply.github.com>
2024-04-27 09:51:01 -0400

    work on browse

---
+++ browse.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Two Owls Cafe</title>
+        <style type=text/css>
+        </style>
+    </head>
+    <body>
+    <h1>BROWSE</h1>
+    
+    </body>
+</html>
