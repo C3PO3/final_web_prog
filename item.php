8fb42ca817304b656102126ef2fdcec371a5cb90
wmagann <157910260+wmagann@users.noreply.github.com>
2024-04-30 10:35:38 -0400

    update

--- item.php
+++ item.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
         <title>View Book</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css" type="text/css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
         </script>
+        <?php include 'header.php'; ?>
         <script>
             $(document).ready(function() {
                 $(".add").click(function() {
@@ -32,6 +31,7 @@
                 width: 240px;
                 object-fit: cover;
                 align-self: flex-start;
+                border-radius: 5px;
             }
 
             .infoFlex {
@@ -97,7 +97,6 @@
             }
 
         </style>
-        <?php include 'header.php'; ?>
     </head>
     <body>
         <div class="container">
